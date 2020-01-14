9e805e03ed3895915229404691ef1e8123eef581
Konstantin <kostyabashkov@gmail.com>
2020-01-14 11:26:48 +0300

    123

---
+++ src/components/Header.svelte
@@ -0,0 +1,46 @@
+<header>
+    <div class="header_box">
+        <div class="total_sum">512000 руб</div>
+        <div class="main_data">
+            <div class="main_data_item">
+                <div class="title">Доход</div>
+                <div class="main_data_item_value profit">+ 5500 руб</div>
+            </div>
+            <div class="main_data_item">
+                <div class="title">Расход</div>
+                <div class="main_data_item_value loss">- 800 руб</div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<style lang="scss">
+    header {
+        background: #aea9f2;
+        color: #fff;
+        padding: 50px;
+    }
+
+    .total_sum{
+        text-align: center;
+        font-size: 50px;
+        font-weight: bold;
+        margin-bottom: 50px;
+    }
+
+    .main_data{
+        display: flex;
+        justify-content: space-around;
+    }
+
+    .main_data_item{
+        .profit{
+            color: green;
+        }
+    }
+
+    .title{
+        font-size: 25px;
+    }
+
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,25 @@
+<svelte:head>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700&display=swap&subset=cyrillic" rel="stylesheet">
+</svelte:head>
+
+<script>
+	import Header from '../components/Header.svelte';
+</script>
+
+<div class="wrapper">
+	<Header />
+	<main>
+
+	</main>
+	<footer></footer>
+</div>
+
+<style>
+	.wrapper{
+		max-width: 768px;
+		margin: 0 auto;
+		border: 1px solid;
+		font-family: 'Roboto Condensed', sans-serif;
+	}
+</style>
