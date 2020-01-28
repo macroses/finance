7cccc51039d4157896212f6b0568d138e3b72c96
konstantin <kostyabashkov@gmail.com>
2020-01-28 22:56:56 +0300

    include content into the page

---
+++ src/routes/category_item/[idPage].svelte
@@ -0,0 +1,14 @@
+<script context="module">
+	export async function preload(page) {
+        const {idPage} = page.params;
+		return { idPage };
+    }
+</script>
+
+<script>
+    export let idPage;
+    let arrFromStore = JSON.parse(localStorage.getItem('arr')).filter(e => e.id == idPage);
+    const { id, name, visible } = arrFromStore[0];
+</script>
+
+<p>новая страница {name}</p>
