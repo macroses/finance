9b86b090782db1aff65dfe0c74385374dc4c109b
Konstantin <kostyabashkov@gmail.com>
2020-01-28 18:03:38 +0300

    some fixes

--- src/routes/category_item/[id].svelte
+++ src/routes/category_item/[id].svelte
@@ -1,13 +1,23 @@
 <script context="module">
+    let idPage;
 	export async function preload(page, session) {
-		let idNum = page.params;
-        let data = JSON.parse(localStorage.getItem('arr'))
-		return console.log(data);;
+        idPage = page.params.id;
+        let data = JSON.parse(localStorage.getItem('arr'));
+		return idPage;
     }
+
+    function getCategoryContent() {
+        let arrFromStore = JSON.parse(localStorage.getItem('arr'));
+        let elemItem = arrFromStore.filter(e => e.id == idPage);
+        return console.log(elemItem[0]);
+    } 
+
+
 </script>
 
 <script>
-    export let pageId;
+    
+
 </script>
 
-<p >новая страница {idNum}</p>
+<p on:click={getCategoryContent}>новая страница {idPage}</p>
